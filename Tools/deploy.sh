4e5400553f1db4a001c4e08972abbcec030e8986
Belgiumboy123 <brechtmoulin@Brechts-MBP.hsd1.wa.comcast.net>
2015-06-12 00:31:01 -0700

    Adds a deploy script and all needed resources
    Creates a deploy script that will take the app bundle Qt Creator
    produces and adds all the needed resources and adds the Qt libraries to
    app.  The script will output a ready-for-user app.
    
    This also adds a setup.sh script to run when first setting up the
    project so the Qt created app bundles will have the proper resources.
    
    However the user needs to run chmod u+x setup.sh /deploy.sh to be able
    to run the scripts.  It maybe useful to write these scripts in python
    to avoid having to change permissions.

---
+++ Tools/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "Starting deployment-------------------------"
+
+# needed directories
+DEPLOY_DIR=$(pwd)
+BUILD_DIR=../Jeopardy-build/Release/Jeopardy
+RESOURCES_DIR=../Resources
+
+# blow away the old saved app
+rm -rf Jeopardy.app
+
+echo "Start copying over the app bundle and all its resources"
+
+# copy the app bundle to deploy directory
+cp  -f -R $BUILD_DIR/Jeopardy.app $DEPLOY_DIR
+
+# copy the needed resources into the app bundle resources
+cp -f $RESOURCES_DIR/clues.db $DEPLOY_DIR/Jeopardy.app/Contents/Resources
+cp -f $RESOURCES_DIR/song.mp3 $DEPLOY_DIR/Jeopardy.app/Contents/Resources
+cp -f $RESOURCES_DIR/AppIcon.icns $DEPLOY_DIR/Jeopardy.app/Contents/Resources
+
+# copy over the info.plist into app bundle
+cp -f $RESOURCES_DIR/Info.plist $DEPLOY_DIR/Jeopardy.app/Contents
+
+echo "Finished with copying"
+
+echo "Running macdeployqt..."
+
+# Dynamically attach all needed Qt libraries to the app
+macdeployqt $DEPLOY_DIR/Jeopardy.app
+
+echo "All Done------------------------------------"
---
+++ Tools/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Please open up Qt Creator, go to Projects tab on the left hand pane."
+echo "Update the build directory to workspace_root/Jeopardy-build/(Debug or Release)"
+echo "Then build both Debug and Release mode."
+echo "Note have not yet figured out how to automate the above steps."
+echo "Copying over needed resouce files to both Debug and Release app bundles..."
+
+RESOURCES_DIR=../Resources
+BUILD_DIR_R=../Jeopardy-build/Release/Jeopardy
+BUILD_DIR_D=../Jeopardy-build/Debug/Jeopardy
+
+cp -f $RESOURCES_DIR/clues.db $BUILD_DIR_R/Jeopardy.app/Contents/Resources
+cp -f $RESOURCES_DIR/song.mp3 $BUILD_DIR_R/Jeopardy.app/Contents/Resources
+
+cp -f $RESOURCES_DIR/clues.db $BUILD_DIR_D/Jeopardy.app/Contents/Resources
+cp -f $RESOURCES_DIR/song.mp3 $BUILD_DIR_D/Jeopardy.app/Contents/Resources
+
+echo "Done. Now should be able to launch and run app in Qt Creator."
