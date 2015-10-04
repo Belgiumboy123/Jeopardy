eb84e7218a6f2f185dab8028e47426e54865adc8
Belgiumboy123 <brecht.moulin@gmail.com>
2015-10-04 01:09:53 -0700

    Add deploy script for server

---
+++ Tools/deploy-server.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Starting Server app deployment-------------------"
+
+# needed directories
+DEPLOY_DIR=$(pwd)
+BUILD_DIR=../Jeopardy-build/Qt5.3/Release/JeopardyServerUI
+RESOURCES_DIR=../Resources
+
+# blow away the old server saved app
+rm -rf JeopardyServer.app
+
+echo "Start copying over the app bundle and all its resources"
+
+# copy the app bundle to deploy directory
+cp -f -R $BUILD_DIR/JeopardyServer.app $DEPLOY_DIR
+
+# copy the needed resources into app bundle resources
+cp -f $RESOURCES_DIR/Server.icns $DEPLOY_DIR/JeopardyServer.app/Contents/Resources
+
+# copy over the info.plist into app bundle
+cp -f $RESOURCES_DIR/Server_Info.plist $DEPLOY_DIR/JeopardyServer.app/Contents/Info.plist
+
+echo "Finished with copying"
+
+echo "Running macdeployqt..."
+
+#Dynamically attach all needed Qt libraries to the app
+macdeployqt $DEPLOY_DIR/JeopardyServer.app
+
+echo "All Done------------------------------------"
